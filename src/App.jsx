04b72587be9b206d9ccc995bfa2369ec24482129
jongtelles <jongtelles@gmail.com>
2022-09-09 12:22:49 -0600

    added GraphQL mutation to create a new user by email and associated error message and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { gql, useMutation } from "@apollo/client";
 import logo from "./assets/logo.svg";
 import illustration from "./assets/illustration.svg";
 import chevron from "./assets/chevron.svg";
@@ -5,11 +7,59 @@ import "./App.scss";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Button } from "./components/Button/Button";
 
+// Define mutation
+const CREATE_NEW_USER = gql`
+  mutation CreateNewUser($name: String!, $username: String!, $email: String!) {
+    createUser(input: { name: $name, username: $username, email: $email }) {
+      name
+      username
+      email
+    }
+  }
+`;
+
+const isValidEmail = email => {
+  return /\S+@\S+\.\S+/.test(email);
+};
+
 function App() {
+  const [addUser] = useMutation(CREATE_NEW_USER);
+  const [error, setError] = useState(null);
+  let input;
+
   const handleSubmit = event => {
     event.preventDefault();
-    alert("email address submitted");
-    // do graphQL stuff
+
+    if (!isValidEmail(input.value)) {
+      setError("Please enter a valid email address");
+      return;
+    }
+
+    addUser({
+      variables: {
+        name: input.value,
+        username: input.value,
+        email: input.value,
+      },
+    })
+      .then(res => {
+        setError(null);
+        input = "";
+        console.log(res.data);
+        // display email to user
+        // I would not normally use an alert - this is just a quick implementation for the exercise
+        // It needs to be wrapped in setTimout to allow the DOM to update and remove the error message
+        setTimeout(() => {
+          alert(
+            `New user created: ${JSON.stringify(
+              Object.values(res.data)[0]["email"],
+              null,
+              4
+            )}`
+          );
+        }, 1);
+      })
+      .catch(error => console.error(error.message));
   };
 
   return (
@@ -33,13 +83,19 @@ function App() {
             fugiat.
           </p>
           <form className='email-form' onSubmit={handleSubmit}>
+            {error && <p className='error-message'>{error}</p>}
             <input
+              ref={node => {
+                input = node;
+              }}
               type='email'
               name='email'
               id='email'
               placeholder='Enter your email'
             />
-            <Button className="indigo">Start free trial</Button>
+            <Button type='submit' className='indigo'>
+              Start free trial
+            </Button>
           </form>
           <p>
             Start your free 14-day trial, no credit card necessary. By providing
